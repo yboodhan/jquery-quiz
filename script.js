b6fed851be80eb8584c90ed4e4cdd18ea7ebc19a
yboodhan <yboodhan@gmail.com>
2019-11-19 16:40:32 -0800

    basic HTML and CSS, start js

---
+++ script.js
@@ -0,0 +1,46 @@
+var quesNum = 0
+var next = 0
+
+$('.start').click(function() {
+    $('#quesAndAns').css('visibility','visible')
+    $('.next').css('visibility', 'visible').click(function() {
+        next++
+        console.log(next)
+        $('.back').css('visibility', 'visible')
+    })
+})
+
+$('.back').click(function() {
+    next--
+    console.log(next)
+})
+
+let answers = []
+
+
+
+
+
+let questions = [{
+    prompt: "What character is is featured in this meme?",
+    answers: ["Sonic", "Spongebob", "Frodo", "Patrick"],
+    correctAnswerIndex: 1
+ }, {
+    prompt: "What video game is this referenced from?",
+    answers: ["Call of Duty", "Fifa", "GTA", "Fortnite"],
+    correctAnswerIndex: 2
+ }, {
+    prompt: "What tv show is this character from?",
+    answers: ["The Simpsons", "Rick & Morty", "Bob's Burgers", "Family Guy"],
+    correctAnswerIndex: 0
+ }, {
+    prompt: "Which this character?",
+    answers: ["Peter griffin", "Bender", "Fry", "Gary"],
+    correctAnswerIndex: 2
+ }, {
+    prompt: "What  is the color of the cat?",
+    answers: ["Black", "White", "Orange", "Pink"],
+    correctAnswerIndex: 1
+ }]
+ 
+ console.log(questions)
