90f19e8d9171fe1d66aae6eb0d295bb96dda719b
yboodhan <yboodhan@gmail.com>
2019-11-19 18:19:06 -0800

    added basic logic for tracking ans

--- script.js
+++ script.js
@@ -1,26 +1,3 @@
-var quesNum = 0
-var next = 0
-
-$('.start').click(function() {
-    $('#quesAndAns').css('visibility','visible')
-    $('.next').css('visibility', 'visible').click(function() {
-        next++
-        console.log(next)
-        $('.back').css('visibility', 'visible')
-    })
-})
-
-$('.back').click(function() {
-    next--
-    console.log(next)
-})
-
-let answers = []
-
-
-
-
-
 let questions = [{
     prompt: "What character is is featured in this meme?",
     answers: ["Sonic", "Spongebob", "Frodo", "Patrick"],
@@ -30,17 +7,55 @@ let questions = [{
     answers: ["Call of Duty", "Fifa", "GTA", "Fortnite"],
     correctAnswerIndex: 2
  }, {
-    prompt: "What tv show is this character from?",
+    prompt: "What TV show is this character from?",
     answers: ["The Simpsons", "Rick & Morty", "Bob's Burgers", "Family Guy"],
     correctAnswerIndex: 0
  }, {
-    prompt: "Which this character?",
-    answers: ["Peter griffin", "Bender", "Fry", "Gary"],
+    prompt: "Who this character?",
+    answers: ["Peter Griffin", "Bender", "Fry", "Gary"],
     correctAnswerIndex: 2
  }, {
     prompt: "What  is the color of the cat?",
     answers: ["Black", "White", "Orange", "Pink"],
     correctAnswerIndex: 1
  }]
+ 
+var quesNum = 0
+var next = 0
+let userAnswers = []
+
+//after clicking start, make quiz and next button visible
+$('.start').click(function() {
+    $('#quesAndAns').css('visibility','visible')
+    $('.next').css('visibility', 'visible')
+})
+
+
+//add event listener to next button that changes next value
+$('.next').click(function() {
+    next++
+    console.log(next)
+})
+
+//add event listener to back button that changes next value
+$('.back').click(function() {
+    next--
+    console.log(next)
+})
+
+//show back button only after ques 1
+
+
+
+//add event listener to each answer choice
+//add user choice to index of array matching question number
+$('.ansChoice').each(function() {
+    $(this).click(function() {
+        var index = $('.ansChoice').index($(this))
+        userAnswers[next] = index
+        console.log(index + " is index")
+        console.log(userAnswers)
+    })
+})
  
  console.log(questions)
